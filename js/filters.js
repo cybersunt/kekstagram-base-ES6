32f0da5168993e13847bc9a98a0e54e7df16e5b8
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-04-21 13:37:55 +0300

    Настроила работу фильтров

--- js/filters.js
+++ js/filters.js
@@ -7,10 +7,10 @@ const pictureZoomingValue = editingWindow.querySelector(`.scale__control--value`
 const filters = editingWindow.querySelector(`.effects`);
 const toggleSlider = editingWindow.querySelector(`.effect-level__pin`);
 const effectsLevel = editingWindow.querySelector(`.effect-level`);
-// const sliderBar = editingWindow.querySelector(`.effect-level__line`);
-// const sliderBarFill = editingWindow.querySelector(`.effect-level__depth`);
+const sliderBar = editingWindow.querySelector(`.effect-level__line`);
+const sliderBarFill = editingWindow.querySelector(`.effect-level__depth`);
 
-// let currentFilterValue = 1;
+let currentFilterValue = 1;
 let currentFilter = `none`;
 
 const settingsEffects = {
@@ -45,12 +45,14 @@ const setFilter = (evt) => {
   if (evt.target.checked) {
     currentFilter = evt.target.value;
     editingWindowFilters.className = `effects__preview--` + currentFilter;
+    toggleSlider.style.left = constants.DEFAULT_EFFECT_LEVEL;
+    sliderBarFill.style.width = constants.DEFAULT_EFFECT_LEVEL;
     utils.removeClassName(effectsLevel, `hidden`);
     setFilterSaturation(constants.DEFAULT_FILTER_VALUE);
   }
 };
 
-const getCurrentFilterValue = (filter, filterValue) => (filter.MAX - filter.MIN) * filterValue;
+const getCurrentFilterValue = (filter, filterValue) => filter.MIN + (filter.MAX - filter.MIN) * filterValue;
 
 const setDefaultSettings = () => {
   pictureZoomingValue.value = constants.SCALE_PERCENTS + `%`;
@@ -82,14 +84,58 @@ const checkUseFilter = (filterName, filterValue) => {
   }
 };
 
+const onMouseDown = (evt) => {
+  evt.preventDefault();
+
+  const SLIDER_WIDTH = toggleSlider.offsetWidth;
+
+  const LimitMovementX = {
+    min: sliderBar.offsetLeft - SLIDER_WIDTH,
+    max: sliderBar.offsetLeft + sliderBar.offsetWidth - SLIDER_WIDTH
+  };
+
+  let startCoordsX = evt.clientX;
+
+  const onMouseMove = (moveEvt) => {
+    moveEvt.preventDefault();
+    let shiftSlider = startCoordsX - moveEvt.clientX;
+
+    let toggleSliderCoord = toggleSlider.offsetLeft - shiftSlider;
+
+    startCoordsX = moveEvt.clientX;
+
+    if (toggleSliderCoord < LimitMovementX.min) {
+      toggleSliderCoord = LimitMovementX.min;
+    } else if (toggleSliderCoord > LimitMovementX.max) {
+      toggleSliderCoord = LimitMovementX.max;
+    }
+
+    toggleSlider.style.left = toggleSliderCoord + 'px';
+    sliderBarFill.style.width = toggleSliderCoord + 'px';
+
+    currentFilterValue = toggleSliderCoord / (LimitMovementX.max - LimitMovementX.min);
+    setFilterSaturation(currentFilterValue);
+  };
+
+  const onMouseUp = (upEvt) => {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+}
+
 const applyEffect = () => {
   filters.addEventListener(`click`, setFilter);
-  toggleSlider.addEventListener(`mouseup`, setFilterSaturation);
+  toggleSlider.addEventListener(`mousedown`, onMouseDown);
 };
 
 const cancelEffect = () => {
   filters.removeEventListener(`click`, setFilter);
-  toggleSlider.removeEventListener(`mouseup`, setFilterSaturation);
+  toggleSlider.removeEventListener(`mousedown`, onMouseDown);
 };
 
 export {applyEffect, cancelEffect};
